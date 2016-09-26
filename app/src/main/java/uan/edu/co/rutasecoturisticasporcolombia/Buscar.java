0f68dafa4434d057e3a4c4d646262e8ed99c0db2
zetasergio <sergmoreno@uan.edu.co>
2016-09-26 16:30:19 -0500

    Actualizar spinner
    Categorias en spinner

--- app/src/main/java/uan/edu/co/rutasecoturisticasporcolombia/Buscar.java
+++ app/src/main/java/uan/edu/co/rutasecoturisticasporcolombia/Buscar.java
@@ -7,7 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class Buscar extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +23,28 @@ public class Buscar extends AppCompatActivity {
         setContentView(R.layout.activity_buscar);
 
 
+        Spinner spn_dto = (Spinner) findViewById(R.id.spn_dpt);
 
-        Spinner spindepa = (Spinner) findViewById(R.id.spn_dpt);
+
+        /*String[] valores = {};
+        spn_dto.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, valores));
+        spn_dto.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
+            {
+                Toast.makeText(adapterView.getContext(), (String) adapterView.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+                // vacio
+
+            }
+        });
+        //spn_dto.setOnItemClickListener();
+        */
 
         Button btn_buscar = (Button) findViewById(R.id.btn_buscar);
 
@@ -31,7 +59,9 @@ public class Buscar extends AppCompatActivity {
         });
 
 
-
-
     }
 }
+
+
+
+
